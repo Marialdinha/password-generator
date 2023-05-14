8b07b4b7f0d9ac331ae499cc33ab3ea6e449d677
Marialdinha <marialda@bellsouth.net>
2023-05-13 19:36:38 -0500

    Added createPassword function

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
 //Write Password function
 function writePassword(){
-    var password = getPasswordLength();
+    var password = null;
+    var passwordLength = getPasswordLength();
+    var isLowerCase = getLowerCaseOption();
 
     //Creating password
-    password = "User selected " + password + " letters"
+    password = "User selected " + password +  " letters, and lowercase = " + isLowerCase 
+    // password = createPassword(passwordLength,isLowerCase);
+
 
     document.getElementById("password").value = password
 }
+
 
 //Get Password Length function
 function getPasswordLength(){
     var passwordLength = prompt("How many letters in your password? (Choose between 10 and 128)");
 
-if (passwordLength < 10 || passwordLength > 128) {
-    alert("Try Again")
+    // need to catch cancel
+    // passwordLength is passing previous value
+if (passwordLength < 10 || passwordLength > 128){
+    alert("Try Again (Choose between 10 and 128)")
     getPasswordLength();
 } 
     return passwordLength;
 
+}
+
+
+// Get Password Lowercase Option
+function getLowerCaseOption(){
+    // How to get yes or no prompt
+    var isLowerCase = confirm("Press OK if you want lower cases charactrs?)");
+
+    return isLowerCase;
+}
+
+function createPassword(passwordLength,isLowerCase){
+    var numbers = "0123456789";
+    var lower = "abcdefghijklmnopqrstuvxz";
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVXZ";
+    var special = "!@#$%^&*()"
+
+    var chars = null
+    if (isLowerCase) {chars += lower}
+
+    var finalPassword = null;
+
+    for (var i = 0; i <= passwordLength; i++){
+        var randomcharacter = Math.floor(Math.random) * chars.length;
+        finalPassword += chars.substring(randomcharacter, randomcharacter +1)
+
+    }
+
+    return finalPassword
 }
 
 //Clear Password function
@@ -32,23 +68,3 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 var clearBtn = document.querySelector("#clear");
 clearBtn.addEventListener("click", clearPassword)
-
-
-
-
-
-
-
-
-
-
-
-
-//writePassword();
-
-// Show password in text area
-// all samples below work 
-// document.getElementById("password").value = "Test";
-// document.querySelector("#password").value = "Test2222222";
-//document.querySelector("textarea").value = "Test33333333";
-
